fdc2833f1122f48b38f9360fbf89e7b6e5049da1
Ryoya Tamura <lollipop@simochee.net>
2025-07-28 15:07:46 +0900

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,17 +1,17 @@
-const querySelectorAsync = async (selector, parentNode = document) => {
+export const querySelectorAsync = async (selector, parentNode = document) => {
       const element = parentNode.querySelector(selector);
       if (element) return element;
-
+      
       return new Promise((resolve) => {
           const observer = new MutationObserver((_, obs) => {
               const element = parentNode.querySelector(selector);
-
+      
               if (element) {
                   obs.disconnect();
                   resolve(element);
               }
           });
-
+      
           observer.observe(parentNode, { childList: true, subtree: true });
       });
-  }
+}
